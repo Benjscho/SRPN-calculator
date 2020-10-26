ec929a9fd4fdcc90dc942d5994447af34ade136b
Benjscho <benjamin.j.schofield+coding@gmail.com>
2020-10-26 17:06:04 +0000

    Additional functionality
    Additional functionality up to the beginning of 4. included. Currently
    not working is continual string input, e.g., "11+1+1+d", comments,
    unexpected inputs, or random numbers.

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def parse(arguments):
         This function iterates through the operands in each statement and
         performs the functions accordingly.
     '''
-    legal_operators = ['+', '-', '*', '^', '/', '%']
+    legal_operators = ['+', '-', '*', '/', '%', '^']
     special_operators = ['=','#','d','r']
     for i in arguments:
         try:
             # If value is an integer push to the stack
-            val = int(i)
+            val: int = int(i)
             if len(main_stack) < main_stack.maxlen:
                 main_stack.append(val)
             else:
@@ -22,20 +22,43 @@ def parse(arguments):
             pass
 
         if i in legal_operators:
-            operands(i)
+            if len(main_stack) > 1:
+                val1 = main_stack.pop()
+                val2 = main_stack.pop()
+                operands(val1, val2, i)
+            else:
+                print("Stack underflow.")
         if i == '=':
-            print(main_stack[-1])
+            try:
+                print(main_stack[-1])
+            except:
+                print("Stack empty.")
         if i == 'd':
             for i in main_stack:
                 print(i)
 
 
-def operands(operator):
-    val1 = main_stack.pop()
-    val2 = main_stack.pop()
-    expression = str(val2) + operator + str(val1)
-    main_stack.append(eval(expression))
-
+def operands(val1, val2, operator):
+    if operator == '^':
+        result = pow(val2, val1)
+        if result > 2147483647:
+            result = 2147483647
+        elif result < -2147483648:
+            result = -2147483648
+        main_stack.append(result)
+    else:
+        expression = str(val2) + operator + str(val1)
+        try:
+            result = int(eval(expression))
+            if result > 2147483647:
+                result = 2147483647
+            elif result < -2147483648:
+                result = -2147483648
+            main_stack.append(result)
+        except ZeroDivisionError:
+            print("Divide by 0.")
+            main_stack.append(val2)
+            main_stack.append(val1)
 
 
 def main():
