72102a9a5e9af7caa08860ea6ea57a737395c867
Benjscho <benjamin.j.schofield+coding@gmail.com>
2020-11-05 15:00:09 +0000

    Final updates

--- srpn.py
+++ srpn.py
@@ -33,8 +33,8 @@ random_int_counter = 0
 
 def overflow_check(value):
     """
-        This function provides a standin to check whether the value is within
-        the int limits in the original sprn
+    This function checks whether the value is within the overflow in the original srpn calculator. If it is outside
+    that range, the saturated value is returned.
     """
     if value > 2147483647:
         value = 2147483647
@@ -45,8 +45,8 @@ def overflow_check(value):
 
 def push_to_stack(val):
     """
-        Checks if the stack is less than the max stack length, and if so
-        pushes the value to the stack after checking for overflows.
+    Checks if the stack is less than the max stack length, and if so
+    pushes the value to the stack after checking for overflows.
     """
     if len(main_stack) < main_stack.maxlen:
         main_stack.append(overflow_check(val))
@@ -56,17 +56,17 @@ def push_to_stack(val):
 
 def exceptional_parse(input_to_parse):
     """
-        Define an exceptional parse function for items that aren't covered
-        by the typical parse where input is split by whitespace. This parses
-        input character by character to build up a split expression that
-        will be parsed.
+    Define an exceptional parse function for items that aren't covered
+    by the typical parse where input is split by whitespace. This parses
+    input character by character to build up a split expression that
+    will be parsed.
 
-        When input is not space separated, The parse order of the version to
-        emulate runs:
-        1) If it's a number, push it to the stack
-        2) If it's a mathematical operator, perform them in reverse input order
-        3) If it's a legal special operator perform it
-        4) If it's anything else print the error message.
+    When input is not space separated, The parse order of the version to
+    emulate runs:
+    1) If it's a number, push it to the stack
+    2) If it's a mathematical operator, perform them in reverse input order
+    3) If it's a legal special operator perform it
+    4) If it's anything else print the error message.
     """
     global legal_operators
     # Initialise temporary holders for numbers, and operators.
@@ -101,22 +101,24 @@ def exceptional_parse(input_to_parse):
                 else:
                     temp_nums.append(temp_number)
                     temp_number = ""
-
-                if len(temp_nums) == 2:
+                # This logic seems to solve some edge cases, while failing others. Needs further troubleshooting.
+                if len(temp_nums) > 2:
                     temp_exp += temp_nums
                     temp_exp += temp_ops[::-1]
                     temp_nums = []
                     temp_ops = []
 
-            # If character is in the legal operators, add to the temporary operstors
+            # If character is in the legal operators, add to the temporary operators
             if j in legal_operators:
                 temp_ops.append(j)
+            # This logic also is required for some edge cases.
             elif j == "=":
                 temp_exp += temp_nums
                 temp_exp.append(j)
                 temp_exp += temp_ops[::-1]
                 temp_nums = []
                 temp_ops = []
+            # Provide handling for comment flags in continuous strings (these are unrecognised in original SRPN)
             elif j == "#":
                 print("Unrecognised operator or operand \"" + j + "\".")
             elif j != " ":
@@ -126,6 +128,7 @@ def exceptional_parse(input_to_parse):
                 temp_nums = []
                 temp_ops = []
 
+    # Logic to reconcile any temporary number, and concatenate the final split expression to be returned and parsed.
     if temp_number != "":
         temp_nums.append(temp_number)
 
@@ -135,30 +138,14 @@ def exceptional_parse(input_to_parse):
 
 def parse(element):
     """
-        This function iterates through the operands in each statement and
-        performs the functions accordingly.
+    This function iterates through the operands in each statement and
+    performs the functions accordingly.
     """
     global legal_operators
     global random_ints
     global random_int_counter
     global comment_flag
 
-    # Check if number is octal, if so, convert from octal to int and continue
-    '''
-        This check is slightly ugly so it requires explanation. First
-        the code checks if this element is flagged as an octal. Numbers are
-        flagged as octal by the prefix '0'. If a number is prefixed '0' it is
-        parsed as an octal. In order to do that the element is split into a
-        list, checks if that list has more than two elements, then checks if
-        the first element is a 0, or if the first two elements are -0, if so
-        it tries to convert the element from a string to an octal.
-
-        The difficulty is bad octals e.g., 09, are ignored by the sprn. So
-        to mimic that behaviour, if an exception is thrown by the octal, but
-        it could be parsed as a regular int the parse function exits with a 1
-        error.
-
-    '''
     # If comment symbol received switch comment flag and skip input until
     # the next comment symbol is received. Stop processing for '#' element.
     if element == '#':
@@ -167,13 +154,27 @@ def parse(element):
     if comment_flag:
         return
 
+    # Check if number is octal, if so, convert from octal to int and continue
+    '''
+    First the code checks if this element is flagged as an octal. Numbers are
+    flagged as octal by the prefix '0'. If a number is prefixed '0' it is
+    parsed as an octal. In order to do that the element is split into a
+    list, checks if that list has more than two elements, then checks if
+    the first element is a 0, or if the first two elements are -0, if so
+    it tries to convert the element from a string to an octal.
+
+    The difficulty is a bad octal (e.g., 0109) is ignored by the SRPN. So
+    to mimic that behaviour, if an exception is thrown by the octal, but
+    it could be parsed as a regular int the parse function exits with a 1
+    error.
+    '''
     octal_check = list(element)
     if len(octal_check) > 1:
-        octal_bools = [
+        octal_boolean = [
             (octal_check[0] == '0'),
             (octal_check[0] == '-' and octal_check[1] == '0')]
 
-        if octal_bools[0] or octal_bools[1]:
+        if octal_boolean[0] or octal_boolean[1]:
             try:
                 element = int(element, 8)
             except:
@@ -201,7 +202,7 @@ def parse(element):
         if '^' in element:
             element = element.replace("^", "**")
             try:
-                assert main_stack[-1] >= 1
+                assert main_stack[-1] >= 0
             except:
                 print("Negative power.")
                 return 0
@@ -231,8 +232,10 @@ def parse(element):
         return 0
 
     # Parse 'r' to push random number on stack. For the first 23 calls
-    # this will print the 23 randoms that are always produced by the SPRN to
+    # this will print the 23 randoms that are always produced by the SRPN to
     # emulate. After that a random int between 1 and 2147483647 is returned.
+    # I believe the original function is rand() from the C++ standard library
+    # but there was no easy way for me to port that.
     elif element == 'r':
         if random_int_counter < 23:
             r = random_ints[random_int_counter]
@@ -254,11 +257,14 @@ def parse(element):
 
 def operands(val1, val2, operator):
     """
-        This function takes two values popped from the stack, and seeks to
-        perform the function indicated by the operator on them. It then pushes
-        the result of that operation back onto the stack.
+    This function takes two values popped from the stack, and seeks to
+    perform the function indicated by the operator on them. It then pushes
+    the result of that operation back onto the stack.
     """
-    # Exceptional case for power operator to call pow function.
+    # This is exceptional handling for when the power is 0. Even if val2 is negative, the result is always positive 1
+    # in the original srpn.
+    if operator == "**" and val1 == 0:
+        val2 = 1
     expression = str(val2) + operator + str(val1)
     try:
         result = int(eval(expression))
@@ -271,16 +277,16 @@ def operands(val1, val2, operator):
 
 def main():
     """
-        The main function starts the calculator then while it is running
-        accepts input. The input is split by whitespaces, the parser then
-        attempts to parse each element. If the parser can parse that element
-        it will return 0.
+    The main function starts the calculator then while it is running
+    accepts input. The input is split by whitespaces, the parser then
+    attempts to parse each element. If the parser can parse that element
+    it will return 0.
 
-        If it gets to the end and the parser cannot parse the element it is
-        likely that there is no whitespace and it is a complicated expression.
-        That element is then passed to the exceptional parser which returns an
-        array of elements split based on conditions that mimic the behaviour of
-        the srpn.
+    If it gets to the end and the parser cannot parse the element it is
+    likely that there is no whitespace and it is a complicated expression.
+    That element is then passed to the exceptional parser which returns an
+    array of elements split based on conditions that mimic the behaviour of
+    the SRPN.
     """
     print("You can now start interacting with the SRPN calculator")
     while True:
@@ -303,7 +309,7 @@ def main():
 
 def test_main(calculation):
     """
-    Function that imitates the main function on a test calculation input.
+    Function that imitates the main function on a test calculation input, called by test_srpn.py.
     """
     # Reset global variables that change so testing is consistent
     global random_int_counter
--- test_srpn.py
+++ test_srpn.py
@@ -1,4 +1,4 @@
-import sprn
+import srpn
 
 from io import StringIO
 import sys
@@ -7,15 +7,19 @@ import sys
 Code snippet to capture standard output for validation testing taken from
 https://stackoverflow.com/questions/16571150/how-to-capture-stdout-output-from-a-python-function-call
 """
+
+
 class Capturing(list):
     def __enter__(self):
         self._stdout = sys.stdout
         sys.stdout = self._stringio = StringIO()
         return self
+
     def __exit__(self, *args):
         self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
+        del self._stringio  # free up some memory
         sys.stdout = self._stdout
+
 
 def test_1():
     x = """
@@ -24,8 +28,9 @@ def test_1():
         +
         =
         """
-    result = sprn.test_main(x)
+    result = srpn.test_main(x)
     assert result[0] == 12
+
 
 def test_2():
     x = """
@@ -34,8 +39,9 @@ def test_2():
         -
         =
         """
-    result = sprn.test_main(x)
+    result = srpn.test_main(x)
     assert result[0] == 8
+
 
 def test_3():
     x = """
@@ -44,8 +50,9 @@ def test_3():
         *
         =
         """
-    result = sprn.test_main(x)
+    result = srpn.test_main(x)
     assert result[0] == 36
+
 
 def test_4():
     x = """
@@ -54,8 +61,9 @@ def test_4():
         /
         =
         """
-    result = sprn.test_main(x)
+    result = srpn.test_main(x)
     assert result[0] == 3
+
 
 def test_5():
     x = """
@@ -64,8 +72,9 @@ def test_5():
         %
         =
         """
-    result = sprn.test_main(x)
+    result = srpn.test_main(x)
     assert result[0] == 2
+
 
 # -- Page two
 
@@ -80,8 +89,9 @@ def test_6():
         +
         =
         """
-    result = sprn.test_main(x)
+    result = srpn.test_main(x)
     assert result[0] == 25
+
 
 def test_7():
     x = """
@@ -95,8 +105,9 @@ def test_7():
         d
         =
         """
-    result = sprn.test_main(x)
+    result = srpn.test_main(x)
     assert result[0] == 7035
+
 
 # -- Page three
 
@@ -107,8 +118,9 @@ def test_8():
         +
         =
         """
-    result = sprn.test_main(x)
+    result = srpn.test_main(x)
     assert result[0] == 2147483647
+
 
 def test_9():
     x = """
@@ -120,8 +132,9 @@ def test_9():
         -
         =
         """
-    result = sprn.test_main(x)
+    result = srpn.test_main(x)
     assert result[0] == -2147483648
+
 
 def test_10():
     x = """
@@ -133,9 +146,10 @@ def test_10():
         =
         """
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert result[0] == 100000
     assert output == ["100000", "Stack underflow.", "100000"]
+
 
 # -- Page four
 
@@ -145,9 +159,10 @@ def test_11():
         +
         """
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert result[0] == 1
     assert output == ["Stack underflow."]
+
 
 def test_12():
     x = """
@@ -158,18 +173,20 @@ def test_12():
         /
         """
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert result[-1] == 0
     assert output == ["Divide by 0."]
+
 
 def test_13():
     x = """
         11+1+1+d
         """
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert result[-1] == 13
     assert output == ["Stack underflow.", "13"]
+
 
 def test_14():
     x = """
@@ -178,7 +195,7 @@ def test_14():
         d
         """
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert result[-1] == 3
     assert output == ["3"]
 
@@ -188,62 +205,64 @@ def test_15():
         3 3 ^ 3 ^ 3 ^=
         """
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert result[-1] == 2147483647
     assert output == ["3"]
+
 
 def test_16():
     x = "rrrrrrrrrrrrrrrrrrrrrrdrrrd"
     y = ["1804289383",
-        "846930886",
-        "1681692777",
-        "1714636915",
-        "1957747793",
-        "424238335",
-        "719885386",
-        "1649760492",
-        "596516649",
-        "1189641421",
-        "1025202362",
-        "1350490027",
-        "783368690",
-        "1102520059",
-        "2044897763",
-        "1967513926",
-        "1365180540",
-        "1540383426",
-        "304089172",
-        "1303455736",
-        "35005211",
-        "521595368",
-        "Stack overflow.",
-        "Stack overflow.",
-        "1804289383",
-        "846930886",
-        "1681692777",
-        "1714636915",
-        "1957747793",
-        "424238335",
-        "719885386",
-        "1649760492",
-        "596516649",
-        "1189641421",
-        "1025202362",
-        "1350490027",
-        "783368690",
-        "1102520059",
-        "2044897763",
-        "1967513926",
-        "1365180540",
-        "1540383426",
-        "304089172",
-        "1303455736",
-        "35005211",
-        "521595368",
-        "1804289383"]
+         "846930886",
+         "1681692777",
+         "1714636915",
+         "1957747793",
+         "424238335",
+         "719885386",
+         "1649760492",
+         "596516649",
+         "1189641421",
+         "1025202362",
+         "1350490027",
+         "783368690",
+         "1102520059",
+         "2044897763",
+         "1967513926",
+         "1365180540",
+         "1540383426",
+         "304089172",
+         "1303455736",
+         "35005211",
+         "521595368",
+         "Stack overflow.",
+         "Stack overflow.",
+         "1804289383",
+         "846930886",
+         "1681692777",
+         "1714636915",
+         "1957747793",
+         "424238335",
+         "719885386",
+         "1649760492",
+         "596516649",
+         "1189641421",
+         "1025202362",
+         "1350490027",
+         "783368690",
+         "1102520059",
+         "2044897763",
+         "1967513926",
+         "1365180540",
+         "1540383426",
+         "304089172",
+         "1303455736",
+         "35005211",
+         "521595368",
+         "1804289383"]
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert output == y
+
 
 # -- My own tests
 def test_17():
@@ -251,16 +270,17 @@ def test_17():
         3dr-=d
         """
     y = ["3",
-        "1804289383",
-        "-1804289380"]
+         "1804289383",
+         "-1804289380"]
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert output == y
+
 
 def test_18():
     x = "2#"
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert output == ["Unrecognised operator or operand \"#\"."]
     assert result[0] == 2
 
@@ -268,9 +288,10 @@ def test_18():
 def test_19():
     x = "0105-345"
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert output == []
     assert result[-1] == -276
+
 
 def test_20():
     x = """
@@ -278,32 +299,52 @@ def test_20():
         =
         """
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert output == ["Stack empty."]
+
 
 def test_21():
     x = "84567*31-=+"
     y = ["2621577",
-        "Stack underflow.",
-        "Stack underflow."]
+         "Stack underflow.",
+         "Stack underflow."]
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert output == y
     assert result[-1] == 2621577
+
 
 def test_22():
     x = "-0162-561"
     y = []
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert output == y
     assert result[-1] == -675
+
 
 def test_23():
     x = "-14+0^45%"
     y = ["Stack underflow."]
     with Capturing() as output:
-        result = sprn.test_main(x)
+        result = srpn.test_main(x)
     assert output == y
     assert result[-1] == -14
 
+
+def test_24():
+    x = "9----4+123"
+    y = ["Stack underflow.", "Stack underflow."]
+    with Capturing() as output:
+        result = srpn.test_main(x)
+    assert output == y
+    assert result[-1] == -110
+
+
+def test_25():
+    x = "-14+0^"
+    y = ["Stack underflow."]
+    with Capturing() as output:
+        result = srpn.test_main(x)
+    assert output == y
+    assert result[-1] == 1
